fa4231035e6b10d13f2ec439810f5bef50bfd7b6
rajeshwariakarshgowda-svg <rajeshwari.akarshgowda@gmail.com>
2025-08-17 00:02:26 +0530

    Second commit

---
+++ addition.py
@@ -0,0 +1,12 @@
+# addition.py
+# Program to add two numbers entered by user
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Adding numbers
+result = num1 + num2
+
+# Displaying result
+print("The sum is:", result)
